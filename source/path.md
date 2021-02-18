71545b5153e5025798f9b734d25f3277f31b0241
hujiren <389878907@qq.com>
2021-02-18 11:16:43 +0800

    create springboot-graalVM

---
+++ source/path.md
@@ -0,0 +1,3 @@
+
+cl.exe 环境变量
+https://blog.csdn.net/weixin_43997331/article/details/104764873
--- src/main/java/io/jonashackt/springbootgraal/SpringBootHelloApplication.java
+++ src/main/java/io/jonashackt/springbootgraal/SpringBootHelloApplication.java
@@ -1,13 +1,15 @@
 package io.jonashackt.springbootgraal;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(proxyBeanMethods = false)
+
+//@SpringBootApplication(proxyBeanMethods = false)
 public class SpringBootHelloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloApplication.class, args);
+//		SpringApplication.run(SpringBootHelloApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
